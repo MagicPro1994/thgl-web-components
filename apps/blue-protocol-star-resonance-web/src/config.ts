e5e2c9e7812f497988a1a2ea87ce78feb62c0567
lmachens <leon@machens.koeln>
2025-10-08 11:21:28 +0200

    Add Blue Protocol: Star Resonance web app

---
+++ apps/blue-protocol-star-resonance-web/src/config.ts
@@ -0,0 +1,118 @@
+import { type AppConfig } from "@repo/lib";
+
+export const APP_CONFIG: AppConfig = {
+  name: "blue-protocol-star-resonance",
+  title: "Blue Protocol: Star Resonance",
+  domain: "starresonance",
+  supportedLocales: ["en"],
+  appUrl: null,
+  internalLinks: [
+    {
+      title: "Asteria Plains Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Asteria Plains with our interactive maps.",
+      href: "/maps/Asteria%20Plains",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/asteria_plains/preview.webp",
+      linkText: "Explore the Asteria Plains Map",
+    },
+    {
+      title: "Asterleeds Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Asterleeds with our interactive maps.",
+      href: "/maps/Asterleeds",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/asterleeds/preview.webp",
+      linkText: "Explore the Asterleeds Map",
+    },
+    {
+      title: "Bahamar Highlands Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Bahamar Highlands with our interactive maps.",
+      href: "/maps/Bahamar%20Highlands",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/bahamar_highlands/preview.webp",
+      linkText: "Explore the Bahamar Highlands Map",
+    },
+    {
+      title: "Windhowl Canyon Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Windhowl Canyon with our interactive maps.",
+      href: "/maps/Windhowl%20Canyon",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/windhowl_canyon/preview.webp",
+      linkText: "Explore the Windhowl Canyon Map",
+    },
+    {
+      title: "Everfall Forest Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Everfall Forest with our interactive maps.",
+      href: "/maps/Everfall%20Forest",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/everfall_forest/preview.webp",
+      linkText: "Explore the Everfall Forest Map",
+    },
+    {
+      title: "Duskdye Woods Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Duskdye Woods with our interactive maps.",
+      href: "/maps/Duskdye%20Woods",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/duskdye_woods/preview.webp",
+      linkText: "Explore the Duskdye Woods Map",
+    },
+    {
+      title: "Underground District Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Underground District with our interactive maps.",
+      href: "/maps/Underground%20District",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/underground_district/preview.webp",
+      linkText: "Explore the Underground District Map",
+    },
+    {
+      title: "Skimmer's Lair Map",
+      description:
+        "Navigate Blue Protocol: Star Resonance's Skimmer's Lair with our interactive maps.",
+      href: "/maps/Skimmer's%20Lair",
+      iconName: "Map",
+      bgImage:
+        "https://data.th.gl/blue-protocol-star-resonance/map-tiles/skimmer_s_lair/preview.webp",
+      linkText: "Explore the Skimmer's Lair Map",
+    },
+    {
+      href: "/guides",
+      title: "config.internalLinks.guides.title",
+      linkText: "config.internalLinks.guides.linkText",
+      iconName: "BookOpen",
+    },
+    {
+      href: "/guides/Field%20Bosses",
+      title: "Field Bosses",
+      linkText: "Discover all Field Bosses",
+      iconName: "BookOpen",
+    },
+    {
+      href: "/guides/Rare%20Spawns",
+      title: "Rare Spawns",
+      linkText: "Discover all Rare Spawns",
+      iconName: "BookOpen",
+    },
+    {
+      href: "/guides/Engram%20Hub",
+      title: "Engram Hub",
+      linkText: "Discover all Engram Hubs",
+      iconName: "BookOpen",
+    },
+  ],
+  promoLinks: [],
+  externalLinks: [],
+  keywords: ["Bosses", "Guides", "Maps", "Rare Spawns", "Engram Hubs"],
+};
